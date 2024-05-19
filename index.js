8c5e30cce77effe6af260382d600c177c7d6ac0f
ViniciusgoDev <127429045+ViniciusgoDev@users.noreply.github.com>
2024-05-19 10:38:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ function calcularExcesso() {
     const valorPassagem = parseFloat(document.getElementById('valor').value);
 
     const volumeCm3 = altura * largura * profundidade;
-    const volumeDm3 = volumeCm3 / 1000;
+    const volumeTotal = volumeCm3 * 300;
 
 
-    const franquiaVolume = 300; // 300 dm³ permitidos
-    const excessoVolume = Math.max(0, volumeDm3 - franquiaVolume);
+    const franquiaVolume = 300;
+    const excessoVolume = Math.max(0, volumeTotal - franquiaVolume);
     const taxaExcesso = 0.005 * valorPassagem;
-    console.log(taxaExcesso)
+   
 
     const custoExcesso = excessoVolume * taxaExcesso;
 
     document.getElementById('result').innerHTML = `
-        <p class="text-gray-700"><strong>Volume da Bagagem:</strong> ${volumeDm3.toFixed(2)} Decimetros Cubicos</p>
+        <p class="text-gray-700"><strong>Volume total:</strong> ${volumeTotal.toFixed(2)} Decimetros Cubicos</p>
         <p class="text-gray-700"><strong>Excesso de Volume:</strong> ${excessoVolume.toFixed(2)} Decimetros Cubicos</p>
-        <p class="text-gray-700"><strong>Custo por decimetro cubico:</strong> R$ ${taxaExcesso.toFixed(2)}</p>
+        <p class="text-gray-700"><strong>Custo por unidade 0,5%:</strong> R$ ${taxaExcesso.toFixed(2)}</p>
         <p class="text-gray-700"><strong>Custo do Excesso:</strong> R$ ${custoExcesso.toFixed(2)}</p>
     `;
 }
@@ -31,7 +31,7 @@ function calcularExcessoPeso() {
     const custoExcesso = excessoPeso * taxaExcesso;
     document.getElementById('resultPeso').innerHTML = `
         <p class="text-gray-700"><strong>Excesso de Peso:</strong> ${excessoPeso.toFixed(2)} Quilos</p>
-        <p class="text-gray-700"><strong>Valor por Quilo:</strong> ${taxaExcesso.toFixed(2)} Por quilo</p>
+        <p class="text-gray-700"><strong>Valor por Quilo 0.5%:</strong> ${taxaExcesso.toFixed(2)} Por quilo</p>
         <p class="text-gray-700"><strong>Custo do Excesso:</strong> R$ ${custoExcesso.toFixed(2)}</p>
     `;
 }
