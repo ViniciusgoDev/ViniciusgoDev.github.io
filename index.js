b8c2a392e68e55b95105419e3b0b63139bfb029c
ViniciusgoDev <127429045+ViniciusgoDev@users.noreply.github.com>
2024-05-19 14:49:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ function calcularExcesso() {
     const volumeTotal = volumeCm3 * 300;
 
 
-    const franquiaVolume = 300;
-    const excessoVolume = Math.max(0, volumeTotal - franquiaVolume);
+    
     const taxaExcesso = 0.005 * valorPassagem;
    
 
-    const custoExcesso = excessoVolume * taxaExcesso;
+    const custoExcesso = volumeTotal * taxaExcesso;
 
     document.getElementById('result').innerHTML = `
         <p class="text-gray-700"><strong>Volume total:</strong> ${volumeTotal.toFixed(2)} Decimetros Cubicos</p>
-        <p class="text-gray-700"><strong>Excesso de Volume:</strong> ${excessoVolume.toFixed(2)} Decimetros Cubicos</p>
         <p class="text-gray-700"><strong>Custo por unidade 0,5%:</strong> R$ ${taxaExcesso.toFixed(2)}</p>
         <p class="text-gray-700"><strong>Custo do Excesso:</strong> R$ ${custoExcesso.toFixed(2)}</p>
     `;
